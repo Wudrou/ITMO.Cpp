9f4a2144b98b761bde280152f551d35771c34ace
Wudrou <73528461+Wudrou@users.noreply.github.com>
2021-07-30 21:57:51 +0300

    Fix 10.1 control task

--- Lab_10/ControlTask_10.1/Triangle.cpp
+++ Lab_10/ControlTask_10.1/Triangle.cpp
@@ -5,28 +5,34 @@
 
 using namespace std;
 
-Triangle::Triangle(Dot side1, Dot side2, Dot side3)
+Triangle::Triangle(Dot* side1, Dot* side2, Dot* side3)
 {
 	this->a = side1;
 	this->b = side2;
 	this->c = side3;
 }
+Triangle::Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+{
+	a = new Dot(x1, y1);
+	b = new Dot(x2, y2);
+	c = new Dot(x3, y3);
+}
 void Triangle::ShowSides()
 {
-	cout << "Сторона 1: " << a.distanceTo(b) << endl;
-	cout << "Сторона 2: " << b.distanceTo(c) << endl;
-	cout << "Сторона 3: " << c.distanceTo(a) << endl;
+	cout << "Сторона 1: " << a->distanceTo(*b) << endl;
+	cout << "Сторона 2: " << b->distanceTo(*c) << endl;
+	cout << "Сторона 3: " << c->distanceTo(*a) << endl;
 }
 double Triangle::Perimeter()
 {
-	return a.distanceTo(b) + b.distanceTo(c) + c.distanceTo(a);
+	return a->distanceTo(*b) + b->distanceTo(*c) + c->distanceTo(*a);
 }
 double Triangle::Square()
 {
 	double semiPerimeter = this->Perimeter();
-	double side1 = a.distanceTo(b);
-	double side2 = b.distanceTo(c);
-	double side3 = c.distanceTo(a);
+	double side1 = a->distanceTo(*b);
+	double side2 = b->distanceTo(*c);
+	double side3 = c->distanceTo(*a);
 
 	return sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3));
 }
--- Lab_10/ControlTask_10.1/Triangle.h
+++ Lab_10/ControlTask_10.1/Triangle.h
@@ -3,12 +3,12 @@
 
 class Triangle
 {
-private:
-	Dot a;
-	Dot b;
-	Dot c;
 public:
-	Triangle(Dot, Dot, Dot);
+	Dot* a;
+	Dot* b;
+	Dot* c;
+	Triangle(Dot* side1, Dot* side2, Dot* side3);
+	Triangle(double x1, double y1, double x2, double y2, double x3, double y3);
 	void ShowSides();
 	double Perimeter();
 	double Square();
